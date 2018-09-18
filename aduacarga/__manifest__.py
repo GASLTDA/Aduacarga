8435db4c1435fa6b823e6b06a667c5b1250b39e0
janeindiran <janeindiran@gmail.com>
2018-09-19 03:12:35 +0530

    Additional Field for Notes

---
+++ aduacarga/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Aduacarga Additional Fields',
+    'version' : '1.1',
+    'author' : 'Janeindiran',
+    'sequence': 30,
+    'description': """Aduacarga Additional Fields""",
+    'category': 'Accounting',
+    'depends' : ['account'],
+    'installable': True,
+    'website': 'https://janeindiran.com',
+    'application': False,
+    'auto_install': False,
+    'data': [
+        'views/account_invoice.xml'
+     ]
+
+}
